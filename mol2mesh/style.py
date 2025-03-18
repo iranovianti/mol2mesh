39b19b6419dcc80e955dc99c5053dfd6360c8a24
iranovianti <iranovianti.ira@gmail.com>
2025-03-18 14:37:23 +0900

    small refinement

--- mol2mesh/style.py
+++ mol2mesh/style.py
@@ -7,7 +7,6 @@ Written by Ira Novianti
 
 class style_config:
 	ATOM_SCALE = 0.25
-	ATOM_RADII = None
 	BOND_RADIUS = 0.2
 	Rvdw = {'C': 1.7, 'H': 1.2, 'O': 1.55, 'N': 1.6, 'P': 1.95, 'S': 1.8,
 			'Ca': 2.4, 'Mg': 2.2, 'Li': 2.2, 'Na': 2.4, 'K': 2.8,
@@ -16,7 +15,7 @@ class style_config:
 			'Zn': 2.1, 'Si': 2.1, 'B': 1.8, 'Al': 2.1, 'Ni': 2.0, 'Cu': 2.0, 'Se': 1.9, 'As': 2.05,
 			'Ag': 2.1, 'Au': 2.1, 'Pd': 2.05, 'Pt': 2.05, 'Pb': 2.3, 'Hg': 2.05, 'others': 2.0}
 	
-	colors = {'N': 'blue',
+	COLORS = {'N': 'blue',
 			'C': 'gray',
 			'O': 'red',
 			'H': 'white',
@@ -24,18 +23,10 @@ class style_config:
 			'others': 'black'}
 	
 	BOND_COLOR = 'gray'
-	ATOM_COLORS = None
 	
-	def __init__(self):
-		if self.ATOM_RADII:
-			self.atom_radius = {k: self.ATOM_RADII for k in self.Rvdw.keys()}
-		else:
-			self.atom_radius = {k: self.ATOM_SCALE * v for k,v in self.Rvdw.items()}
-		
-		if self.ATOM_COLORS:
-			self.atom_color = {k: self.ATOM_COLORS for k in self.colors.keys()}
-		else:
-			self.atom_color = self.colors
+	def __init__(self, atom_radii=None, atom_colors=None):
+		self.atom_radius = self.atom_radius = atom_radii if atom_radii else {k: self.ATOM_SCALE * v for k, v in self.Rvdw.items()}
+		self.atom_color = self.atom_color = atom_colors if atom_colors else self.COLORS.copy()
 
 class BallStick(style_config):
 	pass
@@ -48,7 +39,9 @@ class Sticks(style_config):
 	COLOR = 'gray'
 	BOND_COLOR = COLOR
 	ATOM_COLORS = COLOR
-
+	
+	def __init__(self):
+		super().__init__(atom_radii={k: self.RADIUS for k in self.Rvdw.keys()}, atom_colors={k: self.COLOR for k in self.COLORS.keys()})
 
 class SpaceFilling(style_config):
 	ATOM_SCALE = 1.
